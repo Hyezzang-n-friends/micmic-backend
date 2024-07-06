11bb6e90ed8a231d674fa4decafdf314f81e7bba
hera <ansaudwls95@naver.com>
2024-07-06 22:34:08 +0900

    CI/CD test 용 수정

--- a.py
+++ a.py
@@ -11,7 +11,7 @@ ns_conf = api.namespace('hello', description='Conference operations')
 class HelloController(Resource):
     def get(self):
         """
-            Edit returns a list of conferences
+            Edit76 returns a list of conferences
         """
 
         return make_response(jsonify({"status": "ok", "data": None}, 200))
